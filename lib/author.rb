36732e0352ce4dad37b34232f52e2308f92c3419
Jonathan Dust <d24dust@hotmail.com>
2018-04-25 17:10:53 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,14 +12,14 @@ class Author
 
   def add_post(post)
     post.author = self
-    @post << post
+    @posts << post
     @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
-    @post << post
+    @posts << post
     @@post_count += 1
   end
 
